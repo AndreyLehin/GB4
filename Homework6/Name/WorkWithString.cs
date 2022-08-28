4126a1f65b6d5da388542cb8cfce10adc83b0570
AndreyLehin <110419129+AndreyLehin@users.noreply.github.com>
2022-08-28 17:03:51 +0300

    Add homeWork6

---
+++ Homework6/Name/WorkWithString.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name
+{
+    public class WorkWithString
+    {
+
+        /// <summary>
+        /// Проверяет логин на корректность ввода
+        /// </summary>
+        /// <param name="messege">Логин</param>
+        /// <returns></returns>
+        public static bool StrValid(string messege)
+        {
+            int a = messege.Length;
+            if (a < 2 || a > 10)
+            {
+                Console.WriteLine("Не верная длина логина");
+                Console.WriteLine();
+                return false;
+            }
+            char[] c = messege.ToCharArray();
+            if (char.IsNumber(c[0]))
+            {
+                Console.WriteLine("Первый симовол не может быть число");
+                Console.WriteLine();
+                return false;
+            }
+            int s;
+            string arr1 = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            int l = arr1.Length;
+            for (int i = 0; i < a; i++)
+            {
+                s = 0;
+                for (int j = 0; j < l; j++)
+                {
+                    if (c[i] == arr1[j])
+                    {
+                        s = 1;
+                    }
+                }
+                if (s == 0)
+                {
+                    Console.WriteLine("Использован недопустимый символ");
+                    Console.WriteLine();
+                    return false;
+
+                }
+            }
+
+            return true;
+
+        }
+
+
+        /// <summary>
+        /// Создает массив из слов текста
+        /// </summary>
+        /// <param name="messege"></param>
+        /// <returns></returns>
+        public static string[] ArrWords(string messege)
+        {
+            messege = messege.Trim();
+            char[] c = {',', '.', ';', ':',' ', '-', '(', ')'};
+            string [] arr1 = messege.Split(c, StringSplitOptions.RemoveEmptyEntries);
+            
+            Console.WriteLine($"{arr1.Length} слов в тексте");
+
+            foreach (var word in arr1)
+            {
+                Console.WriteLine(word);
+            }
+           
+            return arr1;
+        }
+
+        /// <summary>
+        /// Выводит слова без заданного символа в конце
+        /// </summary>
+        /// <param name="arr">массив слов</param>
+        /// <param name="a">заданный символ</param>
+        public static void NoWordWithSimbol(string[]arr, string a)
+        {
+
+            string f;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                f =arr[i].Substring(arr[i].Length - 1);
+                if (f != a)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Выводит слова определенной длины
+        /// </summary>
+        /// <param name="arr">массив слов</param>
+        /// <param name="a">максимальная длина слова</param>
+        public static void AnythingWords(string[]arr, int a)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length <= a)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Выводит первое самое длинное слово и строку из самых длинных строк
+        /// </summary>
+        /// <param name="arr">массив слов</param>
+        public static void MaxWord(string[]arr)
+        {
+
+            int q = 0;
+            int max = arr[0].Length;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i].Length > max)
+                {
+                    max = arr[i].Length;
+                    q = i;
+
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(" Первое самое длинное слово в тексте:");
+            Console.WriteLine(arr[q]);
+            Console.WriteLine();
+
+            Console.WriteLine(" Строка из самых длинных слов");
+            StringBuilder str1 = new StringBuilder(100);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length == max)
+                {
+                    str1.Append($" {arr[i]}");
+
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(str1);
+            
+        }
+
+        /// <summary>
+        /// Сравнивает 2 строки 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool ComparisonWords(string a, string b)
+        {
+            int q = a.Length;
+            int w = b.Length;
+            
+            if (q != w)
+                return false;
+
+            int d;
+            for (int j = 0; j < q; j++)
+            {
+                d = 0;
+                for (int k = 0; k < w; k++)
+                {
+                    if (a[j] == b[k])
+                    {
+                        d = 1;
+                    }
+
+                }
+
+                if (d!=1)
+                    return false;
+            }
+
+            return true;    
+        }
+
+
+        /// <summary>
+        /// Очищает массив от мусора
+        /// </summary>
+        /// <param name="messege"></param>
+        /// <returns></returns>
+        public static string[] ArrWordsGood(string [] messege)
+        {
+            string[] arr1 = new string[messege.Length];
+            for (int i = 0; i < messege.Length; i++)
+            {
+
+                arr1[i] = messege[i].Trim();
+                
+            }
+
+            return arr1;
+        }
+
+        /// <summary>
+        /// Создает массив средних баллов учеников школы (из трех оценок)
+        /// </summary>
+        /// <param name="messege"></param>
+        /// <returns></returns>
+        public static double [] ArrAverScore(string[]messege)
+        {
+            double [] arr = new double[messege.Length];
+            for ( int i = 0; i < arr.Length; i++ )
+            {
+                string gpa = messege[i];
+                gpa = gpa.Substring(gpa.Length - 5);
+              
+                arr[i] = (double.Parse(gpa[0].ToString()) + double.Parse(gpa[2].ToString()) + double.Parse(gpa[4].ToString()))/3;
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Возвращает массив и 3-х наименьших средних баллах по школе
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static double[] MinAverScore(double[]arr)
+        {
+            double min1 = arr[0];
+            double min2 = arr[0];
+            double min3 = arr[0];
+          
+            for ( int i = 1; i < arr.Length; i++ )
+            {
+                if ( arr[i] < min1 )
+                {
+                    min1 = arr[i];
+                    break;
+                }
+
+                if (arr[i] < min2)
+                {
+                    min2 = arr[i];
+                    break;
+                }
+
+                if (arr[i] < min3)
+                {
+                    min3 = arr[i];
+                    break;
+                }
+            }
+
+
+           double [] min ={ min1, min2, min3};
+            Console.WriteLine("Три наименьших средних балла:");
+            Console.WriteLine($"  {min[0]}, {min[1]}, {min[2]}");
+
+            return min;
+
+        }
+
+
+        /// <summary>
+        /// Выводит инф об учениках
+        /// </summary>
+        /// <param name="arrSchool"></param>
+        /// <param name="min"></param>
+        /// <param name="averScore"></param>
+        public static void OutputInform(string[]arrSchool, double[]min, double[] averScore)
+        {
+
+            int q = 0;
+             for ( int i = 0; i < arrSchool.Length; i++ )
+            {
+                if (min[0] == averScore[i])
+                {
+                    q++;
+                    Console.WriteLine($" {q}. {arrSchool[i]}.   Средний балл:  {averScore[i]}");
+                    break;
+                }
+
+            }
+             if ( q < 3 )
+            {
+                for ( int i = 0; i < arrSchool.Length; i++ )
+                {
+                    if (min[1] == averScore[i])
+                    {
+                        q++;
+                        Console.WriteLine($" {q}. {arrSchool[i]}.   Средний балл:  {averScore[i]}");
+                        break;
+                    }
+                }
+
+            }
+
+            if (q < 3)
+            {
+                for (int i = 0; i < arrSchool.Length; i++)
+                {
+                    if (min[2] == averScore[i])
+                    {
+                        q++;
+                        Console.WriteLine($" {q}. {arrSchool[i]}.   Средний балл:  {averScore[i]}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Homework6/Tasks6/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Name;
+
+namespace Tasks6
+{
+    public delegate double Fun(double x, double y);
+    public delegate double Fun2(double x);
+
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            OutPutName.PrintInfo(6);
+            OutPutName.Pause();
+            Console.Clear();
+
+
+             
+            // Задача 1. Работа с делегатом
+
+            OutPutName.CursorOnCenter("Задача 1. Работа с делегатом");
+            OutPutName.Pause();
+
+            Console.WriteLine("Функции: F = a * x^2,  F = a * sin(x)");
+
+            Console.WriteLine("Введите число : a");
+            double a = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Введите число : x");
+            double x = double.Parse(Console.ReadLine());
+
+            Table(MyFun, MyFun2, x, a);
+            OutPutName.Pause();
+
+
+
+
+            // Задача 2. Работа с функциями, файлами и делегатами
+
+            Console.WriteLine (   "Задача 2. Есть две функции. Вам нужно будет выбрать,\n с какой работать, а потом " +
+                "выбрать диапазон,\n в котором нужно будет найти минимум");
+            OutPutName.Pause();
+            Console.WriteLine("выберите функцию: нажмите 1, если    F = 5 * x * x    ; нажмите 2, если F = a * Math.Sin(x)  ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите диапазо для Х. Min и Max");
+            double v = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите шаг для X");
+            double h = double.Parse(Console.ReadLine());
+
+
+            string t;
+            if (n == 1)
+            {
+                t = "5 * x * x";
+
+            }
+            else t = "5 * sin(x)";
+
+            SaveFunc("Data.bin", MyFun3, MyFun4, n, v, b, h);
+            Console.Write($" Минимальное значение из диапазона Х от {v}  до  {b}  функции  ({t})  равно:  ");
+            Console.Write(Load("Data.bin"));
+            OutPutName.Pause();
+
+        }
+
+        public static double MyFun(double a, double x)
+        {
+            return a * x *x;
+        }
+
+        public static double MyFun2(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
+        public static void Table(Fun Q, Fun W, double x, double a)
+        {
+            Console.WriteLine();
+
+            int i =0;
+            if (x > 0)
+            {
+                while (i < x)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($" x = {i}    F = a * x^2 = {Q(a, i)}      F = a * sin(x) = {W(a, i)}");
+                    i++;
+                }
+                Console.WriteLine();
+                Console.WriteLine($" x = {x}    F = a * x^2 = {Q(a, x)}      F = a * sin(x) = {W(a, x)}");
+            }
+
+            if (x < 0)
+            {
+                while (i > x)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($" x = {i}    F = a * x^2 = {Q(a, i)}      F = a * sin(x) = {W(a, i)}");
+                    i--;
+                }
+                Console.WriteLine();
+                Console.WriteLine($" x = {x}    F = a * x^2 = {Q(a, x)}      F = a * sin(x) = {W(a, x)}");
+            }
+
+        }
+
+
+        // Для задачи 2.
+
+        public static double MyFun3 (double x)
+        {
+            return 5 * x * x;
+        }
+
+
+        public static double MyFun4(double x)
+        {
+            return 5 * Math.Sin(x);
+        }
+
+
+
+        /// <summary>
+        /// считывает файл и записывает другой файл в пределах диапазона значений
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="Q">делегат метода</param>
+        /// <param name="W">делегат метода</param>
+        /// <param name="n">переменная 1 или 2</param>
+        /// <param name="a">диапозон мин</param>
+        /// <param name="b">диапозон макс</param>
+        /// <param name="h">шаг</param>
+        public static void SaveFunc (string fileName, Fun2 Q, Fun2 W, int n, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create,FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            if (n == 1)
+            {
+                while (x <= b)
+                {
+                    bw.Write(Q(x));
+                    x += h;
+                }
+            }
+            else
+            {
+                while (x <= b)
+                {
+                    bw.Write(W(x));
+                    x += h;
+                }
+
+            }
+            fs.Close();
+            bw.Close();
+        }
+
+        /// <summary>
+        /// Находит мин значение в файле. Считывает поток
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static double Load (string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length/sizeof(double); i++)
+            {
+                d = br.ReadDouble();
+                if (d < min) min = d;
+
+            }
+
+            br.Close();
+            fs.Close();
+            return min;
+        }
+    }
+}
